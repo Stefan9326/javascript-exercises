7ec847df2813b8d6df87726d0126ee835511df23
Stefan Shambrook <stefshambrook@gmail.com>
2022-11-30 22:00:40 +0000

    Solve reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split('');
+    let result = ''
+    for (i = arr.length - 1; i >= 0; i--) result += arr[i];
+    return result;
 };
 
 // Do not edit below this line
